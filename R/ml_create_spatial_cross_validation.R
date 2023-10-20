ee2ba42d4a80e9171412792b00851c337f874fcf
Marques <marquesel@ns02261607md.ninds.nih.gov.niehs.nih.gov>
2023-10-20 14:39:03 -0400

    refacto ml routines

--- R/ml_create_spatial_cross_validation.R
+++ R/ml_create_spatial_cross_validation.R
@@ -1,4 +1,19 @@
+if (!require(spatialsample)) install.packages('spatialsample')
+library(spatialsample)
+library(yardstick)
 
+#' Create several types of spatial folds for cross validation
+#' (with kmeans spatial clusters, by rectangle blocks, by data source, 
+#' or randomly in space) 
+#'
+#' @param data A datatable of observations
+#' @param metadata a list of characters with obs info 
+#' (eg: county, date, lat, lon, ...)
+#' @param predictors a list of characters names of predictors
+#' @param predicted a character with the name of the predicted variable
+#' @return a tibble object with 3 columns: splits, id and type. 
+#' Tips: to recover dataframe from split, use analysis function
+#' @export
 create_sp_fold <- function(obs_sf) {
   # -- uses k-means clustering
   cluster_llo <- spatial_clustering_cv(obs_sf, v = 10)
@@ -12,6 +27,8 @@ create_sp_fold <- function(obs_sf) {
                                            v = 10
   )
   
+  # add leave counties out?
+  
   autoplot(cluster_llo)
   autoplot(block_llo)
   autoplot(net_llo)
@@ -22,80 +39,14 @@ create_sp_fold <- function(obs_sf) {
   net_llo$type <- "network"
   rnd_llo$type <- "random"
   
-  resamples <-
+  sp_samples <-
     dplyr::bind_rows(
       cluster_llo,
       block_llo,
       net_llo,
       rnd_llo
     )
-  return(resamples)
-}
-
-map_rmse_cv <- function(cv_res) {
-  cv_res %>%
-    unnest(.preds) %>%
-    left_join(cv_rmse, by = c("id", "type")) %>%
-    ggplot(aes(color = .estimate)) +
-    geom_sf(aes(geometry = geometry), alpha = 1) +
-    labs(color = "RMSE") +
-    scale_color_whitebox_c(
-      palette = "muted",
-      labels = scales::label_number(suffix = "ºC"),
-      n.breaks = 8,
-      guide = guide_legend(reverse = TRUE)
-    ) +
-    facet_wrap(vars(type), ncol = 1) +
-    theme(
-      axis.text = element_blank(),
-      plot.caption = element_text(size=10, family="serif"),
-      legend.text = element_text(size=12, family="serif"),
-      legend.title = element_text(size=12, family="serif"),
-      panel.background = element_rect(fill = "white"),
-      panel.grid.major=element_line(colour="grey")
-    )
+  return(sp_samples)
 }
 
 
-map_res_cv <- function(cv_res) {
-  cv_res %>%
-    unnest(.preds) %>%
-    ggplot(aes(color = tmin - .pred)) +
-    geom_sf(aes(geometry = geometry), alpha = 1) +
-    labs(color = "RMSE") +
-    scale_color_whitebox_c(
-      palette = "muted",
-      labels = scales::label_number(suffix = "ºC"),
-      n.breaks = 8,
-      guide = guide_legend(reverse = TRUE)
-    ) +
-    facet_wrap(vars(type), ncol = 1) +
-    theme(
-      axis.text = element_blank(),
-      plot.caption = element_text(size=10, family="serif"),
-      legend.text = element_text(size=12, family="serif"),
-      legend.title = element_text(size=12, family="serif"),
-      panel.background = element_rect(fill = "white"),
-      panel.grid.major=element_line(colour="grey")
-    )
-}
-
-plot_reg_cv <- function(cv_res){
-  cv_res %>%
-    unnest(.preds) %>%
-    ggplot() +
-    geom_point(aes(x = tmin, y = .pred), alpha = 1) +
-    geom_abline(slope = 1, intercept = 0, color = "red") +
-    labs(x = "Observed", y = "Predicted") +
-    scale_x_continuous(breaks = seq(9, 27, by = 1)) +
-    scale_y_continuous(breaks = seq(9, 27, by = 1)) +
-    coord_equal() +
-    facet_wrap(vars(type), ncol = 1) +
-    theme(
-      plot.caption = element_text(size=10, family="serif"),
-      legend.text = element_text(size=12, family="serif"),
-      legend.title = element_text(size=12, family="serif"),
-      panel.background = element_rect(fill = "white"),
-      panel.grid.major=element_line(colour="grey")
-    )
-}

08ca3ffa13f1c69f5ee860c20b81f30f6c4d83b3
Marques <marquesel@almbp02246574.niehs.nih.gov>
2023-12-15 16:39:07 -0500

    clean the monitors-related vignettes

--- R/process_monitors.R
+++ R/process_monitors.R
@@ -96,8 +96,38 @@ format_econet_aws <- function(path) {
   obs <- data.table::data.table(obs)
   obs$tn <- (obs$tn - 32) * 5 / 9
   obs$tx <- (obs$tx - 32) * 5 / 9
-  locs <- unique(obs[, c("id", "name", "network", "lat", "lon", "elev")]) %>%
+  # flag stations with >5% missing data in tn or tx
+  obs_ts_tn <- maditr::dcast(
+    obs[, c("time", "id", "tn")],
+    time ~ id,
+    value.var = "tn"
+  ) %>%
+    xts::as.xts()
+  nb_na_tn <- lapply(obs_ts_tn, FUN = function(x) sum(is.na(x)))
+  nb_na_tn <- as.data.frame(do.call(rbind, nb_na_tn))
+  nb_na_tn <-  cbind(id = rownames(nb_na_tn), nb_na_tn)
+  rownames(nb_na_tn) <- seq_len(nrow(nb_na_tn))
+  names(nb_na_tn)[names(nb_na_tn) == "V1"] <- "nb_na_tn"
+  obs_ts_tx <- maditr::dcast(
+    obs[, c("time", "id", "tx")],
+    time ~ id,
+    value.var = "tx"
+  ) %>%
+    xts::as.xts()
+  nb_na_tx <- lapply(obs_ts_tx, FUN = function(x) sum(is.na(x)))
+  nb_na_tx <- as.data.frame(do.call(rbind, nb_na_tx))
+  nb_na_tx <- cbind(id = rownames(nb_na_tx), nb_na_tx)
+  rownames(nb_na_tx) <- seq_len(nrow(nb_na_tx))
+  names(nb_na_tx)[names(nb_na_tx) == "V1"] <- "nb_na_tx"
+  locs <- unique(obs[, c("id", "name", "network", "lat", "lon", "elev")])
+  locs <- list(locs, nb_na_tx, nb_na_tn) %>%
+    purrr::reduce(dplyr::full_join, by = "id") %>%
     data.table::data.table()
+  obs <- list(obs, nb_na_tx, nb_na_tn) %>%
+    purrr::reduce(dplyr::full_join, by = "id") %>%
+    data.table::data.table()
+  obs <- obs[, na_flag := (nb_na_tn > 5 | nb_na_tx > 5), ]
+  locs <- locs[, na_flag := (nb_na_tn > 5 | nb_na_tx > 5), ]
   return(list(obs = obs, locs = locs))
 }
 

4c396b2089576a15e0e0624e0a32505de1b60fd7
Marques <marquesel@almbp02246574.niehs.nih.gov>
2023-12-18 15:39:08 -0500

    fix problems on open_prediction_grid.R (function + test + vignette)

--- R/open_prediction_grid.R
+++ R/open_prediction_grid.R
@@ -1,29 +1,23 @@
 #' Open prediction grid for a given period
 #'
 #' @param period A vector of "Date" objects
+#' @param data_path a character to data folder
 #' @returns a data.table of prediction points
 #' (each row corresponds to a lat, lon, date)
-open_pred_period <- function(period) {
+open_pred_period <- function(period, data_path) {
   if (class(period) != "Date") {
     stop("period is not a Date")
   }
   period <- as.character(period)
   for (d in period) {
-    file <- paste0(
-      "../input/prediction-grid/",
-      "prediction_grid_points_urb_rur_space_time_covariates_",
-      d,
-      ".csv"
-    )
+    file <- paste0(data_path, "pred_grid_", d, ".tif")
     if (!file.exists(file)) {
       stop(paste0("date ", d, " is not in files"))
     }
   }
   list_pred <- list()
   for (d in period) {
-    pred_d <- fread(file)
-    list_pred <- append(list_pred, list(pred_d))
+    list_pred[[d]] <- terra::rast(file)
   }
-  pred_p <- rbindlist(list_pred, fill = TRUE)
-  return(pred_p = pred_p)
+  return(list_pred = list_pred)
 }
